73238e845bcce9a0ec1eccd50b678baeb8b40497
JoseMa Esparev <esparev@hotmail.com>
2022-01-14 14:13:27 -0600

    styles: Fix wrong class names in css less files
    Add contact component to blog container

--- src/containers/Blog.jsx
+++ src/containers/Blog.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Contact from '@components/Contact';
 import { seeBlogPost } from '../actions/index.action';
 import '@styles/Blog.less';
 
@@ -17,19 +18,60 @@ function Blog(props) {
 
   return (
     <main className='blogpost'>
-      <section className='grid-container blogpost-img-container'>
+      <section className='blogpost__image'>
         <img src={currentBlogPost.cover} alt='Blogpost Cover' />
       </section>
       <section className='blogpost__container'>
-        <div className='grid-container'>
+        <div className='blogpost__grid'>
           <h3>News</h3>
           <article>
             <h1>{currentBlogPost.title}</h1>
             <p>{currentBlogPost.description}</p>
-            <p>{currentBlogPost.description}</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
+              illo vitae minima, numquam modi esse blanditiis, maxime nostrum
+              facere earum qui rem sequi commodi ab velit nesciunt dolore
+              consequatur optio. Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ullam laboriosam sit officiis architecto! Ad
+              vero enim, repudiandae sint, magnam inventore, incidunt dolorem
+              similique quis libero sed beatae ut porro? Cum. Lorem ipsum dolor,
+              sit amet consectetur adipisicing elit. Dolorum, reprehenderit qui
+              magni delectus officiis voluptas praesentium sequi distinctio
+              placeat, harum, consequuntur temporibus amet perspiciatis illum
+              unde quisquam dicta eius. Qui! Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Error sed odit, neque voluptatum,
+              reiciendis dolorum voluptas quisquam cumque ex ipsam, harum
+              temporibus illum. Aliquid voluptates quia velit iure, culpa totam!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              voluptatibus ducimus, illo esse vero nam nemo dolores sequi
+              reprehenderit totam, corrupti consectetur natus quidem ex libero!
+              Ex fugiat illum earum.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+              eius velit voluptatibus voluptatem commodi reprehenderit cum
+              cupiditate quos adipisci quod vitae quibusdam aperiam ducimus
+              quidem doloremque, ratione tempora facere eum? Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Ab possimus minus
+              laudantium! Illum unde consequuntur libero ducimus deleniti cum
+              aperiam veniam deserunt provident? Praesentium, quos. Porro dicta
+              libero dolores rem! Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Tempore voluptas dolorum itaque debitis esse
+              voluptatum! Quia odio sit, fugiat omnis magnam fugit accusamus,
+              pariatur nesciunt minima esse vel deleniti veniam. Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit. Architecto optio
+              dolor ab fugiat blanditiis hic suscipit, nam asperiores minus
+              modi, dignissimos nemo mollitia deleniti distinctio rem molestiae!
+              Sed, minima deleniti! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Dicta, quisquam voluptates, pariatur tempora
+              commodi molestiae laudantium maiores hic tenetur ipsam doloremque
+              debitis. Eveniet deleniti, laudantium recusandae quos molestiae
+              voluptate! Adipisci.
+            </p>
           </article>
         </div>
       </section>
+      <Contact />
     </main>
   );
 }
